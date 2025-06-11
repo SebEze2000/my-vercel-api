2175ebbf59461593701606b034f15baf639b472b
Sebastián <sebeze2000@gmail.com>
2025-06-11 00:27:49 -0300

    rerer

--- api/track-click.js
+++ api/track-click.js
@@ -1,10 +1,7 @@
 export default async function handler(req, res) {
-  const allowedOrigin = 'https://landing2.corleoneteam.site';
-
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
